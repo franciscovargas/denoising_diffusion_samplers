676ac6e48b8e6eef48853b3a197f71661128d250
franciscovargas <vargfran@gmail.com>
2022-11-20 21:31:22 +0000

    Added lineplots facilities

--- dds/data_paths.py
+++ dds/data_paths.py
@@ -3,7 +3,7 @@ from os import path
 
 root_path = path.dirname(path.abspath(path.dirname(__file__)))
 
-data_path = path.join(root_path, "data")
+data_path = path.join(root_path, "dds_data")
 pines_path = path.join(data_path, "fpines.csv")
 lr_sonar_path = path.join(data_path, "ionosphere_full.pkl")
 ion_path =  path.join(data_path, "sonar_full.pkl")
