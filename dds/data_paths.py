38d76dab10336d267ddb048965238b4aeb4aca29
franciscovargas <vargfran@gmail.com>
2023-08-17 13:45:05 +0100

    restored funnel

--- dds/data_paths.py
+++ dds/data_paths.py
@@ -4,7 +4,7 @@ from os import path
 root_path = path.dirname(path.abspath(path.dirname(__file__)))
 
 data_path = path.join(root_path, "dds_data")
-pines_path = path.join(data_path, "fpines.csv")
+pines_path = path.join(data_path, "pines.csv")
 lr_sonar_path = path.join(data_path, "ionosphere_full.pkl")
 ion_path =  path.join(data_path, "sonar_full.pkl")
 vae_path= path.join(data_path, "vae.pickle")
